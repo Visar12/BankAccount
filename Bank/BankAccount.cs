09d43f2cee1a31123d6673e9a206d44091fc79d1
visha <visha@TEC-F-PC11>
2022-06-23 08:55:51 +0200

    Add project files.

---
+++ Bank/BankAccount.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { 
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+
+        private static int accountNumberSeed = 080803;
+
+
+        private List<Transaction> allTransactions= new List<Transaction>();
+
+        public BankAccount(string name,decimal initalBalance)
+        {
+            Owner = name;
+            MakeDeposit(initalBalance, DateTime.Now, "initual balance");
+            Number =accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            //hvis  amount er midnre end 0 hvis jeg prøver at sæte negative penge  
+            if (amount <= 0)
+            {
+
+                // throw new Arguments- Arguemt er (decimal amount, DateTime date, string note)
+                // Så den sørger for at amount  skal være højre  eller  lige med 0 
+                //hvis den er mindre end 0 så er den negative og vi throw  exception  så exeption stopper programet
+                // ligesom hvis du vil gerne hæve   -10 kr   fra en hæveautomat så vil  der ske en fejl   så  der er  hvad man kalder en exeptional situation
+                throw new ArgumentException(nameof(amount), "Amount of deposit must be positive");
+                    }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {  
+           
+            if (amount <= 0)
+            {
+           
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive ");
+
+            }
+            // hvis beløbet du hæver vil gør at din saldo vil være 0 
+            if (Balance - amount < 0)
+            {
+                //hvis du  laver en throw så ville programet stop det 
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal ");
+            }
+            // så nu alle funktioner ville stop så vi ikke forlader bare  if statment  og fordi den der throw er der  den ville gør at vi forlade programmet 
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+            report.AppendLine("Date\tAmmount\t Note");
+            foreach (var item in allTransactions)
+            {
+                report.AppendLine($"{item.Date}\t{item.Amount}\t{item.Notes}");
+            }
+            return report.ToString();
+        }
+    }
+}
+
---
+++ Bank/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account =new BankAccount("kendra ",10000);
+            Console.WriteLine($"Account {account.Number} was created  for {account.Owner} with {account.Balance}.");
+         
+            account.MakeWithdrawal(120, DateTime.Now, "Nike");
+            Console.WriteLine(account.Balance);
+
+            account.MakeDeposit(120, DateTime.Now, "stealing");
+          
+            account.MakeWithdrawal(50, DateTime.Now, "Sko");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.GetAccountHistory());
+          
+            //try
+            //{
+            //    account.MakeWithdrawal(120, DateTime.Now, "Attempt to make A withdrawl");
+            //}
+            //catch (ArgumentOutOfRangeException e)
+            //{
+            //    Console.WriteLine("Exception caught creating account with negative balance ");
+            //    Console.WriteLine(e.ToString());
+            //}
+          
+            Console.ReadLine();
+        }
+    }
+}
